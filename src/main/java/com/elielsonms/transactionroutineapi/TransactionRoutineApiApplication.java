31f661bcbf164d810388a3ee0c68b18607095cfd
Edileness <86696879+Edileness@users.noreply.github.com>
2023-01-18 08:23:22 -0300

    feat: Spring Boot startup - Adding Gradle stratucture - Adding SpringBoot initial dependencies

---
+++ src/main/java/com/elielsonms/transactionroutineapi/TransactionRoutineApiApplication.java
@@ -0,0 +1,13 @@
+package com.elielsonms.transactionroutineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionRoutineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionRoutineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elielsonms/transactionroutine/TransactionRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elielsonms.transactionroutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
